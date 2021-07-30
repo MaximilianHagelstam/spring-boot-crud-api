b661cdd6ed75c187b6e066ac0ec20d80b63151c0
MaximilianHagelstam <maximilian.hagelstam@gmail.com>
2021-07-30 23:02:52 +0300

    Initial commit

---
+++ src/main/java/com/maximilian/springbootcrudapi/SpringBootCrudApiApplication.java
@@ -0,0 +1,12 @@
+package com.maximilian.springbootcrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootCrudApiApplication.class, args);
+  }
+}
